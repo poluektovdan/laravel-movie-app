4b10f48353e62480e60a99f2b3b29471933bffa7
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-07-28 16:31:24 +0300

    Добавлена интерактивность элементам страниц сайта с помощью alpinejs

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Movie App</title>
+	<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 	@vite('resources/css/app.css')
 </head>
 <body class="font-sans bg-gray-900 text-white">
